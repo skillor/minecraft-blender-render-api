7380995a15d037bbf01378dbd12cbf9b62737674
skillor <skillor@gmx.net>
2022-07-20 10:30:20 +0200

    feat: match regex skin

--- minecraf_blender_render/skin.py
+++ minecraf_blender_render/skin.py
@@ -1,8 +1,13 @@
 import requests
 import json
+import re
 
 
 def get_skin_bytes(player_name):
+    match = re.match('([a-zA-Z0-9_]{3,16})', player_name)
+    if match is None:
+        raise NameError(f'invalid player name: {player_name}')
+    player_name = match.group()
     response = requests.get(f'https://api.mojang.com/users/profiles/minecraft/{player_name}')
     if response.status_code != 200:
         raise NameError(f'something went wrong while fetching skin for {player_name}')
